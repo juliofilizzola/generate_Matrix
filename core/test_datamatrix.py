aebed0f3baf0b6e878a88da270ec09044d312010
Julio Filizzola <juliofilizzola@hotmail.com>
2024-12-24 09:58:19 -0300

    Streamline test code by removing unnecessary comments
    Removed redundant comments explaining obvious test behaviors to improve code readability and maintainability. This change ensures the test logic is straightforward and less cluttered with excessive explanations.

--- core/test_datamatrix.py
+++ core/test_datamatrix.py
@@ -12,12 +12,10 @@ def datamatrix_generator():
 
 
 def test_get_file_path(datamatrix_generator):
-    """Teste para o método _get_file_path."""
     file_name = "test_file"
     path = datamatrix_generator._get_file_path(file_name)
     assert path == "output/test_file.png"
 
-    # Testando com diretório vazio
     datamatrix_generator.output_directory = ""
     path = datamatrix_generator._get_file_path(file_name)
     assert path == "test_file.png"
@@ -25,8 +23,7 @@ def test_get_file_path(datamatrix_generator):
 
 @patch("your_module.encode")
 def test_create_data_matrix_image(mock_encode, datamatrix_generator):
-    """Teste para o método _create_data_matrix_image."""
-    # Mock para o retorno da função encode
+
     mock_encoded_data = MagicMock()
     mock_encoded_data.width = 10
     mock_encoded_data.height = 10
@@ -39,7 +36,6 @@ def test_create_data_matrix_image(mock_encode, datamatrix_generator):
     assert isinstance(image, Image.Image)
     assert image.size == (10, 10)
 
-    # Testando para entrada inválida (texto vazio)
     with pytest.raises(ValueError, match="Input data is empty. Please provide valid input for DataMatrix generation."):
         datamatrix_generator._create_data_matrix_image("")
 
@@ -47,8 +43,6 @@ def test_create_data_matrix_image(mock_encode, datamatrix_generator):
 @patch("your_module.Image.Image.save")
 @patch("your_module.DatamatrixGenerator._create_data_matrix_image")
 def test_generate(mock_create_image, mock_save, datamatrix_generator):
-    """Teste para o método generate."""
-    # Configurações do mock
     mock_image = MagicMock()
     mock_create_image.return_value = mock_image
     mock_save.return_value = None
@@ -57,14 +51,11 @@ def test_generate(mock_create_image, mock_save, datamatrix_generator):
     file_name = "test_file"
     datamatrix_generator.generate(text, file_name)
 
-    # Verificando se _create_data_matrix_image foi chamado com o texto correto
     mock_create_image.assert_called_once_with(text)
 
-    # Verificando se save foi chamado no caminho correto
     expected_path = "output/test_file.png"
     mock_image.save.assert_called_once_with(expected_path)
 
-    # Teste de erro no método generate
     mock_create_image.reset_mock()
     mock_create_image.side_effect = Exception("Mocked exception")
 
