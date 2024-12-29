1b4640f90cc16ec02c6631a1b7386f8edc8882ae
julio_filizzola <juliofilizzola@hotmail.com>
2024-12-29 13:55:28 -0300

    test: finish

--- core/datamatrix.py
+++ core/datamatrix.py
@@ -1,11 +1,12 @@
 from pylibdmtx.pylibdmtx import encode
-from errors.error_handler import execute_with_error_handling
+
+from errors.error_handler import ErrorHandlerCustom
 from PIL import Image
 
 
 class DatamatrixGenerator:
 
-    def __init__(self, output_directory=""):
+    def __init__(self, output_directory: str = "") -> None:
         self.output_directory = output_directory
 
     def generate(self, text, file_name) -> None:
@@ -16,15 +17,13 @@ class DatamatrixGenerator:
             image.save(file_path)
             print(f"DataMatrix successfully generated and saved at: {file_path}")
         except Exception as e:
-            print(f"Error generating DataMatrix: {e}")
-            execute_with_error_handling(e, "Error generating DataMatrix")
+            ErrorHandlerCustom.handle_error(e, "Error generating DataMatrix")
 
     @staticmethod
     def _create_data_matrix_image(text: str) -> Image:
-        if not text:
+        if len(text) == 0:
             raise ValueError("Input data is empty. Please provide valid input for DataMatrix generation.")
         encoded_data_matrix = encode(text.encode('utf-8', 'ignore'))
-
         return Image.frombytes('RGB', (encoded_data_matrix.width, encoded_data_matrix.height), encoded_data_matrix.pixels)
 
     def _get_file_path(self, file_name: str) -> str:
